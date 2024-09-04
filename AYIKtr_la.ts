699d0c9c72ae183528739893a4528c6fce48b42c
volkean <volkean@gmail.com>
2024-09-04 18:33:29 +0300

    update readme and translation file. add screenshots.

--- AYIKtr_la.ts
+++ AYIKtr_la.ts
@@ -1,152 +1,312 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="tr_TR">
+<TS version="2.1" language="tr_TR">
 <context>
-    <name>ayikOption</name>
+    <name>AyikOption</name>
     <message>
-        <location filename="ayikoption.cpp" line="9"/>
-        <location filename="ayikoption.cpp" line="32"/>
-        <location filename="ayikoption.cpp" line="40"/>
-        <location filename="ayikoption.cpp" line="190"/>
+        <location filename="ayikoption.cpp" line="21"/>
+        <location filename="ayikoption.cpp" line="45"/>
+        <location filename="ayikoption.cpp" line="49"/>
+        <location filename="ayikoption.cpp" line="216"/>
         <source>Tag</source>
         <translation>Etiket</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="9"/>
+        <location filename="ayikoption.cpp" line="21"/>
         <source>Name</source>
         <translation>İsim</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="9"/>
-        <location filename="ayikoption.cpp" line="193"/>
+        <location filename="ayikoption.cpp" line="21"/>
+        <location filename="ayikoption.cpp" line="219"/>
         <source>Rating</source>
         <translation>Seviye</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="9"/>
+        <location filename="ayikoption.cpp" line="21"/>
         <source>Meaning</source>
         <translation>Anlam</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="21"/>
-        <source>Frequency</source>
-        <translation>Uyarı Sıklığı</translation>
-    </message>
-    <message>
-        <location filename="ayikoption.cpp" line="25"/>
-        <source>Frequency(mins)</source>
-        <translation>Sıklık(dk)</translation>
-    </message>
-    <message>
-        <location filename="ayikoption.cpp" line="38"/>
+        <location filename="ayikoption.cpp" line="24"/>
         <source>All</source>
         <translation>Tümü</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="47"/>
+        <location filename="ayikoption.cpp" line="34"/>
+        <source>Frequency</source>
+        <translation>Uyarı Sıklığı</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="38"/>
+        <source>Frequency(mins)</source>
+        <translation>Sıklık(dk)</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="50"/>
+        <source>Choose</source>
+        <translation>Seç</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="69"/>
         <source>Word Level</source>
         <translation>Kelime Seviyesi</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="51"/>
+        <location filename="ayikoption.cpp" line="73"/>
         <source>Level</source>
         <translation>Seviye</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="72"/>
+        <location filename="ayikoption.cpp" line="94"/>
         <source>Add...</source>
         <translation>Ekle...</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="73"/>
+        <location filename="ayikoption.cpp" line="95"/>
         <source>Delete</source>
         <translation>Sil</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="88"/>
+        <location filename="ayikoption.cpp" line="96"/>
+        <source>Delete All</source>
+        <translation>Tümünü Sil</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="97"/>
+        <source>Update</source>
+        <translation>Güncelle</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="125"/>
         <source>Filter:</source>
         <translation>Süz:</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="114"/>
+        <location filename="ayikoption.cpp" line="152"/>
         <source>General</source>
         <translation>Genel</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="115"/>
+        <location filename="ayikoption.cpp" line="153"/>
         <source>Words</source>
         <translation>Kelimeler</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="116"/>
+        <location filename="ayikoption.cpp" line="154"/>
         <source>Help</source>
         <translation>Yardım</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="187"/>
+        <location filename="ayikoption.cpp" line="213"/>
         <source>Open Words File</source>
         <translation>Kelime Dosyası Aç</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="187"/>
+        <location filename="ayikoption.cpp" line="213"/>
         <source>Text Files (*.txt)</source>
         <translation>Metin Dosyaları (*.txt)</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="190"/>
+        <location filename="ayikoption.cpp" line="216"/>
         <source>Tag:</source>
         <translation>Etiket:</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="193"/>
+        <location filename="ayikoption.cpp" line="219"/>
         <source>Rating:</source>
         <translation>Seviye:</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="208"/>
+        <location filename="ayikoption.cpp" line="234"/>
         <source>Deleting..</source>
         <translation>Siliniyor..</translation>
     </message>
     <message>
-        <location filename="ayikoption.cpp" line="208"/>
+        <location filename="ayikoption.cpp" line="234"/>
         <source>Cancel</source>
         <translation>İptal</translation>
     </message>
 </context>
 <context>
-    <name>ayikPopup</name>
+    <name>AyikPopup</name>
     <message>
-        <location filename="ayikpopup.cpp" line="32"/>
+        <location filename="ayikpopup.cpp" line="31"/>
         <source>Close</source>
         <translation>Kapat</translation>
     </message>
     <message>
-        <location filename="ayikpopup.cpp" line="33"/>
+        <location filename="ayikpopup.cpp" line="32"/>
         <source>Next</source>
         <translation>Sonraki</translation>
     </message>
     <message>
-        <location filename="ayikpopup.cpp" line="34"/>
+        <location filename="ayikpopup.cpp" line="33"/>
         <source>Reveal</source>
         <translation>Göster</translation>
     </message>
 </context>
 <context>
-    <name>ayikTray</name>
+    <name>AyikTray</name>
     <message>
-        <location filename="ayiktray.cpp" line="52"/>
+        <location filename="ayiktray.cpp" line="54"/>
         <source>&amp;Options</source>
         <translation>&amp;Ayarlar</translation>
     </message>
     <message>
-        <location filename="ayiktray.cpp" line="55"/>
+        <location filename="ayiktray.cpp" line="57"/>
         <source>&amp;Help</source>
         <translation>&amp;Yardım</translation>
     </message>
     <message>
-        <location filename="ayiktray.cpp" line="58"/>
+        <location filename="ayiktray.cpp" line="60"/>
         <source>&amp;Quit</source>
         <translation>&amp;Çıkış</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="ayikoption.cpp" line="106"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="107"/>
+        <source>Tag</source>
+        <translation>Etiket</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="108"/>
+        <source>Name</source>
+        <translation>İsim</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="109"/>
+        <source>Meaning</source>
+        <translation>Anlam</translation>
+    </message>
+    <message>
+        <location filename="ayikoption.cpp" line="110"/>
+        <source>Rating</source>
+        <translation>Seviye</translation>
+    </message>
+</context>
+<context>
+    <name>ayikOption</name>
+    <message>
+        <source>Tag</source>
+        <translation type="vanished">Etiket</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="vanished">İsim</translation>
+    </message>
+    <message>
+        <source>Rating</source>
+        <translation type="vanished">Seviye</translation>
+    </message>
+    <message>
+        <source>Meaning</source>
+        <translation type="vanished">Anlam</translation>
+    </message>
+    <message>
+        <source>Frequency</source>
+        <translation type="vanished">Uyarı Sıklığı</translation>
+    </message>
+    <message>
+        <source>Frequency(mins)</source>
+        <translation type="vanished">Sıklık(dk)</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation type="vanished">Tümü</translation>
+    </message>
+    <message>
+        <source>Word Level</source>
+        <translation type="vanished">Kelime Seviyesi</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation type="vanished">Seviye</translation>
+    </message>
+    <message>
+        <source>Add...</source>
+        <translation type="vanished">Ekle...</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="vanished">Sil</translation>
+    </message>
+    <message>
+        <source>Filter:</source>
+        <translation type="vanished">Süz:</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="vanished">Genel</translation>
+    </message>
+    <message>
+        <source>Words</source>
+        <translation type="vanished">Kelimeler</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="vanished">Yardım</translation>
+    </message>
+    <message>
+        <source>Open Words File</source>
+        <translation type="vanished">Kelime Dosyası Aç</translation>
+    </message>
+    <message>
+        <source>Text Files (*.txt)</source>
+        <translation type="vanished">Metin Dosyaları (*.txt)</translation>
+    </message>
+    <message>
+        <source>Tag:</source>
+        <translation type="vanished">Etiket:</translation>
+    </message>
+    <message>
+        <source>Rating:</source>
+        <translation type="vanished">Seviye:</translation>
+    </message>
+    <message>
+        <source>Deleting..</source>
+        <translation type="vanished">Siliniyor..</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="vanished">İptal</translation>
+    </message>
+</context>
+<context>
+    <name>ayikPopup</name>
+    <message>
+        <source>Close</source>
+        <translation type="vanished">Kapat</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation type="vanished">Sonraki</translation>
+    </message>
+    <message>
+        <source>Reveal</source>
+        <translation type="vanished">Göster</translation>
+    </message>
+</context>
+<context>
+    <name>ayikTray</name>
+    <message>
+        <source>&amp;Options</source>
+        <translation type="vanished">&amp;Ayarlar</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="vanished">&amp;Yardım</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="vanished">&amp;Çıkış</translation>
     </message>
 </context>
 </TS>
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # AYIK
+
+This is a vocabulary builder application that helps you learn new words in English. 
+Main target is Turkish users striving for memorizing English words. The application runs on system tray and according to a schedule you set, it will randomly as a word on the screen.
+
+Note that codebase needs some maintenance in regard to code quality, e.g. dead codes, inconsistent usages, etc. 
+
+## Tech Stack
+C++, QT, Sqlite
+
+## Screenshots
+![Tray, popup](images/screenshots/ayik-tray-popup.jpg "Tray, popup")
+
+![Settings-General](images/screenshots/ayik-settings-general.jpg "Settings-General")
+
+![Settings-Words](images/screenshots/ayik-settings-words.jpg "Settings-Words")
+
+![Settings-Help](images/screenshots/ayik-settings-help.jpg "Settings-Help")
